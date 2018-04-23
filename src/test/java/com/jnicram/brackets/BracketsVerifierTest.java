51c4dac0f0919b7b6011e25dc4399cd4d190f4e4
Marcin Jasinski <jnicram@gmail.com>
2018-04-23 21:39:06 +0200

    adds parametrized tests

--- src/test/java/com/jnicram/brackets/BracketsVerifierTest.java
+++ src/test/java/com/jnicram/brackets/BracketsVerifierTest.java
@@ -3,10 +3,39 @@ package com.jnicram.brackets;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class BracketsVerifierTest {
 
+    private final String data;
+    private final VerifyResult dataResult;
+
     private BracketsVerifier classUnderTest;
+
+    public BracketsVerifierTest(String data, VerifyResult dataResult) {
+        this.data = data;
+        this.dataResult = dataResult;
+    }
+
+    @Parameters( name = "{index}: verify({0}) = {1}" )
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] {
+                { null , VerifyResult.TRUE },
+                { "", VerifyResult.TRUE },
+                { "(]", VerifyResult.FALSE },
+                { "([3)]", VerifyResult.FALSE },
+                { "([6)", VerifyResult.FALSE },
+                { "(([34[34]])234)", VerifyResult.TRUE },
+                { "([([()])])", VerifyResult.TRUE }
+        };
+        return Arrays.asList(data);
+    }
 
     @Before
     public void init() {
@@ -16,13 +45,11 @@ public class BracketsVerifierTest {
     @Test
     public void shouldReturnTrueStringWhenEmptyStringGiven() {
         // given
-        String data = "";
 
         // when
         String result = classUnderTest.verify(data);
 
         // then
-        Assert.assertEquals(VerifyResult.TRUE.value(), result);
+        Assert.assertEquals(dataResult.value(), result);
     }
-
 }
